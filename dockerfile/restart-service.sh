9b52401dd3e541ee8cf553b75edf868fc99019aa
Jason Chin <jason.chin@wholebiome.com>
2018-02-22 04:22:29 +0000

    add redis-commander into coordinator for development/easy to debug for now. I plan to remove it later.

--- dockerfile/restart-service.sh
+++ dockerfile/restart-service.sh
@@ -12,7 +12,7 @@ docker build -t worker worker-minimum/
 
 docker swarm init
 docker network create --subnet 10.0.0.0/16 --driver overlay cirrus 
-docker service create --hostname coordinator --name coordinator --network cirrus --publish 7000:80 -d coordinator 
+docker service create --hostname coordinator --name coordinator --network cirrus --publish 7000:80 --publish 8081:8081 -d coordinator 
 sleep 1
 docker service create --hostname dispatcher --name dispatcher --network cirrus --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock -d  dispatcher 
-docker service create --hostname seqdb --name seqdb --network cirrus -d redis
+#docker service create --hostname seqdb --name seqdb --network cirrus -d redis
